e947240c7e510828256cfe335c2c0f21bafeec4d
Franco Dans <dansfranco17@gmail.com>
2021-11-30 20:31:44 -0300

    segundo commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default function Item({list}) {
     return (
         <div>
+            <NavLink to = {`/item/${list.id}`}/>
             <h1>{list.id}</h1>
             <h2>{list.name}</h2>
             <h3>{list.status}</h3>
+            
         </div>
     )
 }
---
+++ src/components/ItemDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Item from "./Item";
+
+const ItemDetail = ({item}) => {
+    return (
+        <div>
+            <h2>{list.id} </h2>
+        </div>
+    )
+}
+
+export default ItemDetail
